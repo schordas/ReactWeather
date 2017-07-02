94aa76d1f35a23f5a31205e78d856171ce251e6e
Samuel Chordas <samec39@gmail.com>
2017-07-01 23:42:41 -0700

    Set up for heroku

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-  console.log('Express server is up on port 3000');
+app.listen(port, function(){
+  console.log('Express server is up on port ' + port);
 });
