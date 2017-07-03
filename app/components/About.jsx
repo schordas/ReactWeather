84cee17ea29601f7371adb0d126b7d53f6fae941
Samuel Chordas <samec39@gmail.com>
2017-07-02 21:20:22 -0700

    update about page.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,15 +5,14 @@ var About = (props) => {
 	return (
 		<div>
 			<h1 className="text-center">About</h1>
-			<h2 className="text-center">Created by Sam Chordas.</h2>
-			<p className="text-center">Based on a course by Andrew Mead.</p>
-			<p className="text-center">Check out some of my other stuff here:</p>
+			<p>This app was built with React for The Complete React Web Developer Course.</p>
+			<p>Check out some of my other stuff here:</p>
 			<ol>
 				<li>
-					<a href="github.com/schordas">Github</a>
+					<a href="https://github.com/schordas">Github</a>
 				</li>
 				<li>
-					<a href="mjolnir.io">mjolnir.io</a>
+					<a href="http://mjolnir.io">mjolnir.io</a>
 				</li>
 			</ol>
 		</div>
