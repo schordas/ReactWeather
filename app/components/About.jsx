952d1b665962face4e7e295ef1a7b207d93f3847
Samuel Chordas <samec39@gmail.com>
2017-07-02 21:30:33 -0700

    finalize about page.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -6,15 +6,29 @@ var About = (props) => {
 		<div>
 			<h1 className="text-center">About</h1>
 			<p>This app was built with React for The Complete React Web Developer Course.</p>
+			<p>To build the app I used:</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react">React</a> - This is the JS framework used.
+				</li>
+				<li>
+					<a href="http://openweathermap.org">Open Weather Map</a> - This is the weather API that 
+						powers the lookup.
+				</li>
+				<li>
+					<a href="http://foundation.zurb.com/">Foundation</a> - The UI was built using this
+						front end framework.
+				</li>
+			</ul>
 			<p>Check out some of my other stuff here:</p>
-			<ol>
+			<ul>
 				<li>
 					<a href="https://github.com/schordas">Github</a>
 				</li>
 				<li>
 					<a href="http://mjolnir.io">mjolnir.io</a>
 				</li>
-			</ol>
+			</ul>
 		</div>
 	);
 };
