92fd99a3e15fdbfcd49603a2a49c9603a2b22c59
Samuel Chordas <samec39@gmail.com>
2017-07-02 20:47:27 -0700

    updated about page.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,20 @@
 var React = require('react');
+var {Link} = require()
 
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>Welcome to the About page!</p>
+			<h1 className="text-center">About</h1>
+			<h2 className="text-center">This app was created by Sam Chordas.</h2>
+			<p className="text-center">Check out some of my other stuff here:</p>
+			<ol>
+				<li>
+					<Link to="github.com/schordas">Github</Link>
+				</li>
+				<li>
+					<Link to="mjolnir.io">mjolnir.io</Link>
+				</li>
+			</ol>
 		</div>
 	);
 };
