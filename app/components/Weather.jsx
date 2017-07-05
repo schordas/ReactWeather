438f5edd82038167b22efe64d022dd9e0a68c81b
Samuel Chordas <samec39@gmail.com>
2017-07-04 17:13:43 -0700

    Wired up nav bar search.

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -16,8 +16,11 @@ var Weather = React.createClass({
 		
 		this.setState({
 			isLoading: true,
-			errorMessage: undefined
+			errorMessage: undefined,
+			location: undefined,
+			temp: undefined
 		});
+
 		openWeatherMap.getTemp(location).then(function (results) {
 			that.setState({
 				isLoading: false,
@@ -32,7 +35,22 @@ var Weather = React.createClass({
 		});
 		
 	},
+	componentDidMount: function () {
+		var location = this.props.location.query.location;
 
+		if (location && location.length > 0){
+			this.handleSearch(location);
+			window.location.hash = '#/';
+		}
+	},
+	componentWillReceiveProps: function (newProps){
+		var location = newProps.location.query.location;
+
+		if (location && location.length > 0){
+			this.handleSearch(location);
+			window.location.hash = '#/';
+		}
+	},
 	render: function () {
 		var {isLoading, temp, location, errorMessage} = this.state;
 
